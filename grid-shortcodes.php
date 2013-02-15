2b2eb5de581767341e84fe340c46503311bc0d3d
Evan Mattson <evanmattson+git@gmail.com>
2013-02-14 21:04:00 -0500

    v1.2 update

--- grid-shortcodes.php
+++ grid-shortcodes.php
@@ -4,7 +4,7 @@ Plugin Name: Grid Shortcodes
 Plugin URI: http://evanmattson.pagelines.me/plugins/grid-shortcodes
 Demo: http://evanmattson.pagelines.me/plugins/grid-shortcodes
 Description: Adds a collection of shortcodes for easy implementation of the responsive Bootstrap Grid!
-Version: 1.1
+Version: 1.2
 Author: Evan Mattson
 Author URI: http://evanmattson.pagelines.me
 PageLines: true
@@ -17,7 +17,6 @@ class GridShortcodes {
 		self::add_shortcodes();
 
 		add_filter( 'the_content', array(&$this, 'do_grid_shortcodes'), 7 );
-
 	}
 
 
@@ -61,9 +60,12 @@ class GridShortcodes {
 			'span12'
 		);
 
-		foreach ( $tags as $tag )
+		// now we're going to add a LOT of shortcodes (13*(26+1))... 351
+		foreach ( $tags as $tag ) {
 			add_shortcode( $tag, array(&$this, 'grid_shortcodes') );
-
+			foreach ( self::get_alphabet_array() as $x )
+				add_shortcode( "{$tag}_$x", array(&$this, 'grid_shortcodes') );
+		}
 	}
 
 	/**
@@ -73,18 +75,29 @@ class GridShortcodes {
 
 		extract( shortcode_atts(self::default_atts(), $atts) );
 
+		$grid_class = self::get_grid_class( $tag );
+
 		$content = trim( $content );
 
-		if ( 'row' != $tag )
+		// grid css targets spanX with > selector
+		if ( 'row' != $grid_class )
 			$content = self::maybe_wrap_content( $atts, $content, $tag );
 
-		return sprintf('<div%s class="%s%s">%s</div>',
-			$id ? " id=\"$id\"" : '',
-			$tag,
+		return sprintf('<div %s class="%s%s">%s</div>',
+			$id ? "id='$id'" : '',
+			$grid_class,
 			$class ? " $class" : '',
 			do_shortcode( $content )
 		);
+	}
 
+	function get_grid_class( $tag ) {
+		if ( false !== strpos($tag, '_') ) {
+			$_tag = explode('_', $tag);
+			return $_tag[0];
+		}
+		else
+			return $tag;
 	}
 
 	function maybe_wrap_content( $atts, $content, $tag ) {
@@ -94,7 +107,9 @@ class GridShortcodes {
 			// if the pad class is set use it, otherwise give it a default
 			// pad="" will give the wrapping div an empty class
 			return sprintf('<div class="%s">%s</div>',
-				isset( $atts['pad'] ) ? esc_attr( $atts['pad'] ) : "span-pad {$tag}-pad",
+				isset( $atts['pad'] ) 
+					? esc_attr( $atts['pad'] )
+					: sprintf('span-pad %s-pad', self::get_grid_class( $tag ) ),
 				$content
 			);
 		}
@@ -110,11 +125,10 @@ class GridShortcodes {
 		if ( isset( $atts['pad'] ) )
 			return true;
 
-		// check to see if it was used without a value
-		foreach ( $atts as $key => $value ) {
+		// check to see if it was used without an attribute: value-only
+		foreach ( $atts as $key => $value )
 			if ( is_int( $key ) && 'pad' == $value )
 				return true;
-		}
 
 		return false;
 	}
@@ -131,7 +145,10 @@ class GridShortcodes {
 		);
 	}
 
-
+	function get_alphabet_array() {
+		$alpha = 'a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s-t-u-v-w-x-y-z';
+		return explode('-', $alpha);
+	}
 } // END OF CLASS
 
 new GridShortcodes;
